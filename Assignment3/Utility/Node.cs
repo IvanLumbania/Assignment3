842e7d4b3d03f7da5edeed6382d19fc0934ded27
Ronn Ivan Lumbania <RonnIvan.Lumbania@edu.sait.ca>
2024-03-30 23:56:44 -0600

    first commit

---
+++ Assignment3/Utility/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Utility
+{
+    public class Node
+    {
+        private object element;
+        private Node next;
+
+        public Node(object o)
+        {
+            this.Element = o;
+        }
+
+        public Node(object o, Node n)
+        {
+            this.Element = o;
+            this.Next = n;
+        }
+
+        public object Element { get => element; set => element = value; }
+        public Node Next { get => next; set => next = value; }
+    }
+}
